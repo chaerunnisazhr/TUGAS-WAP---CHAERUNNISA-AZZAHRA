eac35c3e3430ac12b14d7dab04ac02b5a3f931b8
chaerunnisazhr <95474284+chaerunnisazhr@users.noreply.github.com>
2021-12-03 17:04:29 +0700

    Add files via upload

---
+++ inventory/welcome_message.php
@@ -0,0 +1,4 @@
+<div class="container">
+    <h1 class="mt-5 ">Selamat Datang di CeDirectory</h1>
+    <p>Jangan lupa follow Instagram <a href="https://www.instagram.com/chaerunnisazhr/ ">disini</a>.</p>
+</div>
